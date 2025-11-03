78538e607351227de24c4ff0b50b13d7d783b688
franck <pandurofranck@gmail.com>
2025-11-03 10:39:42 -0500

    botones ubicacion, sintomas, funcionan correctamente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import {UsuarioListarComponent} from './componente/usuario-listar-component/usua
 import {UsuarioNuevoEditComponent} from './componente/usuario-nuevo-edit-component/usuario-nuevo-edit-component';
 import {AcercaComponent} from './componente/acerca-component/acerca-component';
 import {InicioComponent} from './componente/inicio-component/inicio-component';
+import {SintomaListarComponente} from './componente/sintoma-listar-componente/sintoma-listar-componente';
+import {SintomaNuevoEditComponente} from './componente/sintoma-nuevo-edit-componente/sintoma-nuevo-edit-componente';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent, pathMatch: 'full' },
@@ -12,5 +14,7 @@ export const routes: Routes = [
   { path: 'nuevo-edit-ubicacion', component: UbicacionNuevoEditComponente},
   { path: 'usuarios', component: UsuarioListarComponent},
   { path: 'nuevo-edit', component: UsuarioNuevoEditComponent},
+  { path: 'sintoma', component: SintomaListarComponente},
+  { path: 'nuevo-edit-sintoma', component: SintomaNuevoEditComponente},
   { path: 'acerca', component: AcercaComponent},
 ];
