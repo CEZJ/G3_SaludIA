bf86a91d19b3947bb67e006ab2e6e1a39345ccda
franck <pandurofranck@gmail.com>
2025-11-03 20:59:21 -0500

    yasta sintomas

--- src/app/componente/sintoma-nuevo-edit-componente/sintoma-nuevo-edit-componente.ts
+++ src/app/componente/sintoma-nuevo-edit-componente/sintoma-nuevo-edit-componente.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
-
+import {ChangeDetectionStrategy, Component, computed, signal} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+export interface Task {
+  name: string;
+  completed: boolean;
+  subtasks?: Task[];
+}
 @Component({
   selector: 'app-sintoma-nuevo-edit-componente',
-  imports: [],
+  imports: [MatCheckboxModule, FormsModule, MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './sintoma-nuevo-edit-componente.html',
   styleUrl: './sintoma-nuevo-edit-componente.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SintomaNuevoEditComponente {
+  readonly task = signal<Task>({
+    name: 'Parent task',
+    completed: false,
+    subtasks: [
+      {name: 'Child task 1', completed: false},
+      {name: 'Child task 2', completed: false},
+      {name: 'Child task 3', completed: false},
+    ],
+  });
 
+  readonly partiallyComplete = computed(() => {
+    const task = this.task();
+    if (!task.subtasks) {
+      return false;
+    }
+    return task.subtasks.some(t => t.completed) && !task.subtasks.every(t => t.completed);
+  });
+
+  update(completed: boolean, index?: number) {
+    this.task.update(task => {
+      if (index === undefined) {
+        task.completed = completed;
+        task.subtasks?.forEach(t => (t.completed = completed));
+      } else {
+        task.subtasks![index].completed = completed;
+        task.completed = task.subtasks?.every(t => t.completed) ?? true;
+      }
+      return {...task};
+    });
+  }
 }
