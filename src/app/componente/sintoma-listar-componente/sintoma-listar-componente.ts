d6c25ce2520fce8368afe7fd6d8bdb7de479af19
franck <pandurofranck@gmail.com>
2025-11-03 22:24:24 -0500

    css nuevos sintomas

--- src/app/componente/sintoma-listar-componente/sintoma-listar-componente.ts
+++ src/app/componente/sintoma-listar-componente/sintoma-listar-componente.ts
@@ -1,11 +1,62 @@
-import { Component } from '@angular/core';
+import {Component, inject, ViewChild} from '@angular/core';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef,
+  MatTable, MatTableDataSource
+} from '@angular/material/table';
+import {MatSort, MatSortHeader} from '@angular/material/sort';
+import {MatPaginator} from '@angular/material/paginator';
+import {DatePipe} from '@angular/common';
+import {Sintoma} from '../../model/sintoma';
+import {SintomaService} from '../../services/sintoma-service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-sintoma-listar-componente',
-  imports: [],
+  imports: [
+    MatPaginator,
+    MatTable,
+    MatSort,
+    MatColumnDef,
+    MatHeaderCell,
+    MatHeaderCellDef,
+    MatCell,
+    MatCellDef,
+    MatHeaderRow,
+    MatHeaderRowDef,
+    MatRow,
+    MatRowDef,
+    MatPaginator,
+    DatePipe,
+    MatSortHeader
+  ],
   templateUrl: './sintoma-listar-componente.html',
   styleUrl: './sintoma-listar-componente.css',
 })
 export class SintomaListarComponente {
-
+  displayedColumns: string[] = ['idSintoma', 'nombre'];
+  dataSource: MatTableDataSource<Sintoma> = new MatTableDataSource<Sintoma>();
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  sintomaService: SintomaService = inject(SintomaService);
+  route : Router = inject(Router);
+  constructor() {
+  }
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+  ngOnInit(){ //se ejecuta al cargar la página
+    console.log("Llamando a mi API");
+    this.sintomaService.list().subscribe({
+      next: data => {
+        console.log("API me ha traido:", data);
+        this.dataSource.data = data;
+      }
+    })
+  }
 }
